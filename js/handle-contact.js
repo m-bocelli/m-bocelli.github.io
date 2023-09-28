4d494f9a6391984f1e9ae5d8e7a4abe3d6e9ab0e
Mikey <michael.j.bocelli@gmail.com>
2023-09-28 11:14:35 -0400

    feat: email validation, must fit format and be non-null

--- js/handle-contact.js
+++ js/handle-contact.js
@@ -1,23 +1,55 @@
-window.onload = () => {
-    document.getElementById('contact-form').addEventListener('submit', (ev) => {
-        ev.preventDefault();
-        (() => {
-            emailjs.init('ztzlIyaNRAdIu1PhQ');
-        })();
+const user_fname = document.getElementById('user_fname');
+const user_lname = document.getElementById('user_lname');
+const user_email = document.getElementById('user_email');
+const user_phone = document.getElementById('user_phone');
+const user_message = document.getElementById('user_message');
+const contact_form = document.getElementById('contact-form');
+const email_error = document.querySelector('#user_email + span.form-error');
+// format: any_text_except_whitespace@any_text_except_whitespace.any_text_except_whitespace
+const email_regex = /^\S{1,}[@]\S{1,}[.]\S{1,}$/g; 
 
-        const form_fields = {
-            user_fname: document.getElementById('fname').value,
-            user_lname: document.getElementById('lname').value,
-            user_email: document.getElementById('email').value,
-            user_phone: document.getElementById('phone').value,
-            user_message: document.getElementById('message').value,
-        };
+// format: 3 digits - 3 digits - 4 digits
+const phone_regex = /^[0-9]\d{2}-[0-9]\d{2}-[0-9]\d{3}$/g; 
 
-//        if(validateEmail() && validatePhone()) {
+contact_form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    (() => {
+        emailjs.init('ztzlIyaNRAdIu1PhQ');
+    })();
+
+    const form_fields = {
+        user_fname: user_fname.value,
+        user_lname: user_lname.value,
+        user_email: user_email.value,
+        user_phone: user_phone.value,
+        user_message: user_message.value
+    };
+    
+    if(validateEmail()) {
         emailjs.send('contact_service', 'contact_form', form_fields)
-            .then(res => console.log(`Email sent: ${res}`))
+            .then(() => console.log(`Email sent.`))
             .catch(err => console.log(`Error sending email: ${err}`));
-//        }
-    });
-};
+    } else {
+        console.log('invalid form');
+    }
+});
+
+function validateEmail() {
+    const is_format = email_regex.test(user_email.value); 
+    const is_empty = user_email.value.length < 1;
+    const is_valid = is_format && !is_empty;
+
+    if (is_valid) {
+        email_error.textContent = '';
+        email_error.className = 'form-error';
+    } else if (!is_format && !is_empty) {
+        email_error.textContent = 'Required format: <username>@<domain>.<top-level domain>'; 
+        email_error.className = 'form-error active';
+    } else {
+        email_error.textContent = 'Email is required.';
+        email_error.className = 'form-error active';
+    }
+
+    return is_valid;
+}
 
