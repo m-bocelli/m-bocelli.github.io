6865f8b673a96b576476c2a2a7f549e96b07973f
Mikey <michael.j.bocelli@gmail.com>
2023-09-28 02:20:40 -0400

    feat: Email is sent when form is submitted

--- js/handle-contact.js
+++ js/handle-contact.js
@@ -1,10 +1,23 @@
-function submitContactForm() {
-    (() => {
-        emailjs.init("ztzlIyaNRAdIu1PhQ");
+window.onload = () => {
+    document.getElementById('contact-form').addEventListener('submit', (ev) => {
+        ev.preventDefault();
+        (() => {
+            emailjs.init('ztzlIyaNRAdIu1PhQ');
+        })();
 
-    })();
+        const form_fields = {
+            user_fname: document.getElementById('fname').value,
+            user_lname: document.getElementById('lname').value,
+            user_email: document.getElementById('email').value,
+            user_phone: document.getElementById('phone').value,
+            user_message: document.getElementById('message').value,
+        };
 
-//    emailjs.sendForm('contact_service', 'contact_form')
+//        if(validateEmail() && validatePhone()) {
+        emailjs.send('contact_service', 'contact_form', form_fields)
+            .then(res => console.log(`Email sent: ${res}`))
+            .catch(err => console.log(`Error sending email: ${err}`));
+//        }
+    });
+};
 
-    console.log('submitted');
-}
